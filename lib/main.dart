2c7a190cc051a43aa54d7bf1a15e70d68b4faaf8
gbmoreira <gbmoreira92@gmail.com>
2023-10-17 21:32:23 -0300

    aula1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Testando meu primeiro App em Flutter'),
         ),
       ),
     );
